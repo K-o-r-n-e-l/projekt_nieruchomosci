d8ca5637221129ad78f5f4b7a84f6ae01108ffcf
K-o-r-n-e-l <kornelwozniak123@gmail.com>
2025-12-16 23:28:08 +0100

    drobna poprawka danych (dodanie unknown)

--- scripts/cleaning.py
+++ scripts/cleaning.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import numpy as np
 from loading_data import loading_data
+import os
 
 
 df_apps, df_rent = loading_data()
@@ -8,8 +8,24 @@ df_apps, df_rent = loading_data()
 
 
 
-print("\nBraki danych (ilość pustych pól):")
+print("\nBraki danych (ilość pustych pól) dla df_apps:")
 print(df_apps.isnull().sum())
 
 
+
+print("\nBraki danych (ilość pustych pól) dla df_rent:")
+print(df_rent.isnull().sum())
+
+
 df_apps = df_apps.fillna("unknown")
+df_rent = df_rent.fillna("unknown")
+
+
+
+if __name__ == '__main__':
+    sciezka_zapis_apps = os.path.join("..", "data", "processed", "mieszkania_sell.csv")
+    sciezka_zapis_apps_rent = os.path.join("..", "data", "processed", "mieszkania_sell_rent.csv")
+    df_apps.to_csv(sciezka_zapis_apps, index=False)
+    print(f"💾 Zapisano połączony plik w: {sciezka_zapis_apps}")
+    df_rent.to_csv(sciezka_zapis_apps_rent, index=False)
+    print(f"💾 Zapisano połączony plik w: {sciezka_zapis_apps_rent}")
